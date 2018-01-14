c48caf99804a96317ec2255959b55b7d6db40009
artur855 <artur855@hotmail.com>
2018-01-14 15:25:04 -0300

    Add markdown post and improve code

--- app/__init__.py
+++ app/__init__.py
@@ -3,25 +3,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from flaskext.markdown import Markdown
 from flask_dotenv import DotEnv
+import flask_debugtoolbar
 import logging
 from logging.handlers import SMTPHandler
 from flask_migrate import Migrate
+from flask_misaka import Misaka
 aplication = Flask(__name__)
 credentials = None
 # mail_handler = SMTPHandler((Config.MAIL_SERVER, Config.MAIL_PORT), 'no-reply@' +
 #                            Config.MAIL_SERVER, Config.ADMINS, 'microblog failure', credentials)
 # mail_handler.setLevel(logging.ERROR)
 # aplication.logger.addHandler(mail_handler)
-markdown = Markdown(aplication)
 env = DotEnv()
 env.init_app(aplication, env_file=os.path.join(
     os.getcwd(), '.env'), verbose_mode=True)
 from config import Config
 db = SQLAlchemy(aplication)
+misaka = Misaka(aplication, math=True, highlight=True)
 aplication.config.from_object(Config)
 migrate = Migrate(aplication, db)
+toolbar = flask_debugtoolbar.DebugToolbarExtension(aplication)
 login = LoginManager(aplication)
 login.login_view = 'login'
 mail = Mail(aplication)
